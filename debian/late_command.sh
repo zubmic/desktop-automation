a067500a9c8dadc078ce075edcd93d048473581c
Michał Żuber <michalzuber22@gmail.com>
2022-09-13 13:35:07 +0200

    Ansible: test different approach to username variable

--- debian/late_command.sh
+++ debian/late_command.sh
@@ -6,4 +6,4 @@ update-alternatives --install /usr/bin/python python /usr/bin/python3 2
 # Clone the repo and run the playbook
 git clone https://github.com/zubmic/desktop-automation.git
 cd desktop-automation
-ansible-playbook ansible/main.yml
+ansible-playbook ansible/main.yml -e desktop_user=$(cat /etc/passwd | grep 1000 | awk -F: '{ print $1 }')
