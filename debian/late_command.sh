36ed276a51ad41b648758a828a34d53df77648fd
Michał Żuber <michalzuber22@gmail.com>
2022-09-13 11:30:09 +0200

    Set Python 3 as default interpreter

--- debian/late_command.sh
+++ debian/late_command.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# Set python3 as the default python
+update-alternatives --install /usr/bin/python python /usr/bin/python3 2
+
+# Clone the repo and run the playbook
 git clone https://github.com/zubmic/desktop-automation.git
 cd desktop-automation
-ansible -m ping localhost
-sleep 20s
+ansible-playbook ansible/main.yml
