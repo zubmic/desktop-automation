73c3aeddb6c932a61d6bd5d720083b42d16d3389
Michał Żuber <michalzuber22@gmail.com>
2022-09-14 10:57:21 +0200

    Ansible: create inventory file

--- debian/late_command.sh
+++ debian/late_command.sh
@@ -5,5 +5,5 @@ update-alternatives --install /usr/bin/python python /usr/bin/python3 2
 
 # Clone the repo and run the playbook
 git clone https://github.com/zubmic/desktop-automation.git
-cd desktop-automation
-ansible-playbook ansible/main.yml -e desktop_user=$(cat /etc/passwd | grep 1000 | awk -F: '{ print $1 }')
+cd desktop-automation/ansible
+ansible-playbook main.yml -i inventory -e desktop_user=$(cat /etc/passwd | grep 1000 | awk -F: '{ print $1 }')
