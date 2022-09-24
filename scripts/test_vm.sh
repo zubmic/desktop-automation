b6906f1f84d14aa18eba67a5df86f88e4f5ceada
Michał Żuber <michalzuber22@gmail.com>
2022-09-24 20:14:56 +0200

    Remove unnecessary if statement

--- scripts/test_vm.sh
+++ scripts/test_vm.sh
@@ -11,22 +11,21 @@ cleanup() {
 # Call the cleanup function if interruption occurs
 trap cleanup INT
 
-if [[ $vm_name == *debian* || $iso_path == *debian* ]]; then
-    virt-install \
-        --accelerate \
-        --check-cpu \
-        --connect=qemu:///system \
-        --disk /tmp/$vm_name.qcow2,size=40,format=qcow2 \
-        --hvm \
-        --location=$iso_path \
-        --name $vm_name \
-        --network=default \
-        --os-variant=generic \
-        --ram 4096 \
-        --vcpus=2 \
-        --wait -1
-fi
+virt-install \
+    --accelerate \
+    --check-cpu \
+    --connect=qemu:///system \
+    --disk /tmp/$vm_name.qcow2,size=40,format=qcow2 \
+    --hvm \
+    --location=$iso_path \
+    --name $vm_name \
+    --network=default \
+    --os-variant=generic \
+    --ram 4096 \
+    --vcpus=2 \
+    --wait -1
 
+# Purge on request
 if [ $2 == "purge" ]; then
     cleanup
 fi
