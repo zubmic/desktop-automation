1244ab4e8b15877c8dbfb64404c9f08a5a27c6ac
Michał Żuber <michalzuber22@gmail.com>
2022-09-24 21:16:40 +0200

    Restructure test_vm.sh script and add more functionalities

--- scripts/test_vm.sh
+++ scripts/test_vm.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+### Manages a VM for testing purposes
 
 vm_name=$1
 iso_path=$2
+disk_file=$3
 
 cleanup() {
     virsh destroy $vm_name
@@ -11,21 +13,42 @@ cleanup() {
 # Call the cleanup function if interruption occurs
 trap cleanup INT
 
-virt-install \
-    --accelerate \
-    --check-cpu \
-    --connect=qemu:///system \
-    --disk /tmp/$vm_name.qcow2,size=40,format=qcow2 \
-    --hvm \
-    --location=$iso_path \
-    --name $vm_name \
-    --network=default \
-    --os-variant=generic \
-    --ram 4096 \
-    --vcpus=2 \
-    --wait -1
-
-# Purge on request
-if [ $2 == "purge" ]; then
-    cleanup
-fi
+case $2 in
+    *.iso)
+        virt-install \
+            --accelerate \
+            --check-cpu \
+            --connect=qemu:///system \
+            --disk /tmp/$vm_name.qcow2,size=40,format=qcow2 \
+            --hvm \
+            --location=$iso_path \
+            --name $vm_name \
+            --network=default \
+            --os-variant=generic \
+            --ram 4096 \
+            --vcpus=2 \
+            --wait -1
+        ;;
+    restore-copy)
+        cp $disk_file /tmp/$vm_name-copy.qcow2
+        virt-install \
+        --accelerate \
+        --check-cpu \
+        --connect=qemu:///system \
+        --disk /tmp/$vm_name-copy.qcow2 \
+        --hvm \
+        --import \
+        --name $vm_name-copy \
+        --network=default \
+        --os-variant=generic \
+        --ram 4096 \
+        --vcpus=2 \
+        --wait -1
+        ;;
+    create-copy)
+        cp $disk_file /var/lib/libvirt/images/$vm_name-copy.qcow2
+        ;;
+    purge)
+        cleanup
+        ;;
+esac
