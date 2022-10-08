89464d1d98a66921deb9a25c8ca37a6d57d7c9a1
Michał Żuber <michalzuber22@gmail.com>
2022-10-08 12:08:50 +0200

    Refactor: Improve copy-to-target script to automate the execution

--- local/copy-to-target.sh
+++ local/copy-to-target.sh
@@ -7,3 +7,6 @@ sshpass -p $pass ssh $host "rm -rf $path/in-target"
 
 # Copy the directory over
 sshpass -p $pass scp -r $(dirname $0)/../in-target $host:$path 
+
+# Run the configure script
+sshpass -p $pass ssh $host "sudo -S $path/in-target/configure.sh"
