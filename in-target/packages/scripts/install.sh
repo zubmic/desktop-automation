9c16f2838a833135fdfa34dd4e65bac831bed5a0
Michał Żuber <michalzuber22@gmail.com>
2022-10-12 23:19:13 +0200

    Create custom mc skin

--- in-target/packages/scripts/install.sh
+++ in-target/packages/scripts/install.sh
@@ -7,21 +7,17 @@ case $distro in
     debian)
         echo "**APT-GET UPDATE**"
         apt-get update -q
-        echo ""
 
         echo "**APT-GET INSTALL**"
         apt-get install -q -y ${install_packages[*]} ${debian_packages[*]}
-        echo ""
 
         echo "**APT-GET AUTOREMOVE**"
         apt-get autoremove -q --purge -y ${remove_packages[*]}
-        echo ""
 
         echo "**CONFIGURE UNATTENDED UPGRADES**"
         echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections
         dpkg-reconfigure -f noninteractive unattended-upgrades 2>/dev/null
         sed -i "s/\/\/Unattended-Upgrade::Mail \"\";/Unattended-Upgrade::Mail \"$username\";/" /etc/apt/apt.conf.d/50unattended-upgrades
-        echo ""
 
         echo "**DPKG INSTALL**"
         for package_url in ${dpkg_packages[*]}; do
@@ -30,6 +26,8 @@ case $distro in
             wget -nc --quiet $package_url -O /tmp/$package_name
             dpkg -u /tmp/$package_name 2>/dev/null
         done
-        echo ""
     ;;
 esac
+
+echo "**INSTALL MC SKIN**"
+cp -v $(dirname $0)/../files/mc/custom.ini /usr/share/mc/skins/
