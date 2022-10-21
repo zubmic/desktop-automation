96401115a8b594749bb6f1b34f83e347d93643d2
Michał Żuber <michalzuber22@gmail.com>
2022-10-21 12:49:54 +0200

    Preseed: encrypt the disk. Prepare-iso: change installation splash image.

--- local/prepare-iso.sh
+++ local/prepare-iso.sh
@@ -31,16 +31,19 @@ if [ $? -eq 0 ]; then
     gzip $copy_dir/install.amd/initrd
     chmod -w -R $copy_dir/install.amd/
 
-    # Add firmware package to the iso
     if [ $distro == 'debian' ]; then
+        # Add firmware package to the iso
         wget -q --show-progress -nc $iwlwifi -P $copy_dir/firmware/dep11
+        
+        # Append boot argument so fdisk can be preloaded and used later in preseed
+        sed -i 's/--- quiet/modules=util-linux-udeb --- quiet/' $copy_dir/isolinux/txt.cfg
+
+        # Change splash screen shown on iso boot
+        cp "$(dirname $0)/files/splash.png" $copy_dir/isolinux/
     fi
 
     # Add scripts used in preseed
     cp -R "$(dirname $0)/../in-target" $copy_dir/
-
-    # Append boot argument so fdisk can be preloaded and used later in preseed
-    sed -i 's/--- quiet/modules=util-linux-udeb --- quiet/' $copy_dir/isolinux/txt.cfg
 
     # Recalculate md5 checksums and update the list
     chmod +w $copy_dir/md5sum.txt
