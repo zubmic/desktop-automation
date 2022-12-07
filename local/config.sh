173caf331bd3c99fc5564500e7e36cddae26f9f2
Michał Żuber <michalzuber22@gmail.com>
2022-12-07 07:59:34 +0100

    Refactor: Simplify the config.sh script

--- local/config.sh
+++ local/config.sh
@@ -2,15 +2,24 @@
 
 copy_dir='/tmp/iso'
 iso_mount='/media/iso'
-disk_file="/var/lib/libvirt/images/"
+disk_file='/var/lib/libvirt/images/'
+distro=$1
 
-if [ $distro == 'debian' ]; then
-    config_name="preseed.cfg"
-    copy_name='debian-preseed-netinst.iso'
-    disk_file+="$vm_name.qcow2"
-    iso_name='debian-11.5.0-amd64-netinst.iso'
-    iso_path="$copy_dir/$copy_name"
+if [ "$distro" == 'debian' ]; then
+    checksum_name='SHA256SUMS'
+    config_name='preseed.cfg'
+    iso_url='https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.5.0-amd64-netinst.iso'
     iwlwifi='http://ftp.es.debian.org/debian/pool/non-free/f/firmware-nonfree/firmware-iwlwifi_20210315-3_all.deb'
-    url='https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/'
-    vm_name=$(echo $iso_name | awk -F- '{ print $1 }')
+elif [ "$distro" == 'fedora' ]; then
+    checksum_name='Fedora-Workstation-36-1.5-x86_64-CHECKSUM'
+    config_name='ks.cfg'
+    iso_url='https://eu.edge.kernel.org/fedora/releases/36/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-36-1.5.iso'
 fi
+
+iso_name=$(basename $iso_url)
+checksum_url="$(echo $iso_url | sed "s/$iso_name//")$checksum_name"
+vm_name=$(echo $iso_name | awk -F- '{ print tolower($1) }')
+disk_file="/var/lib/libvirt/images/$vm_name.qcow2"
+
+copy_name="$vm_name.iso"
+iso_path="$copy_dir/$copy_name"
--- local/prepare-iso.sh
+++ local/prepare-iso.sh
@@ -1,65 +1,65 @@
 #!/bin/bash
-# Prepare iso with preseed configuration
+# Prepare iso with preseed/kickstart configuration
 
 export distro=$1
 source $(dirname $0)/config.sh
 
 # Download iso and verify the checksum
 mkdir -p $iso_mount $copy_dir
-wget -q --show-progress -nc "$url/$iso_name" "$url/SHA256SUMS" -P /tmp
-(cd /tmp && sha256sum -c --ignore-missing SHA256SUMS)
+wget -q --show-progress -nc "$iso_url" "$checksum_url" -P /tmp
+(cd /tmp && sha256sum -c --ignore-missing $(basename $checksum_url))
 
-# If checksum is OK
-if [ $? -eq 0 ]; then
+# # If checksum is OK
+# if [ $? -eq 0 ]; then
 
-    # Remove the modified iso if present from previous run
-    rm -f $copy_dir/$copy_name
+#     # Remove the modified iso if present from previous run
+#     rm -f $copy_dir/$copy_name
 
-    # Create a copy of read-only iso to allow modification
-    mount -o ro /tmp/$iso_name $iso_mount
-    cp -R $iso_mount/. $copy_dir
-    umount $iso_mount
+#     # Create a copy of read-only iso to allow modification
+#     mount -o ro /tmp/$iso_name $iso_mount
+#     cp -R $iso_mount/. $copy_dir
+#     umount $iso_mount
 
-    # Inject the preseed file into initrd
-    chmod +w -R $copy_dir/install.amd/
-    gunzip $copy_dir/install.amd/initrd.gz
-    (
-        cd $(dirname $0)/files && 
-        echo $config_name | 
-        cpio --quiet -H newc -o -A -F $copy_dir/install.amd/initrd
-    )
-    gzip $copy_dir/install.amd/initrd
-    chmod -w -R $copy_dir/install.amd/
+#     # Inject the preseed file into initrd
+#     chmod +w -R $copy_dir/install.amd/
+#     gunzip $copy_dir/install.amd/initrd.gz
+#     (
+#         cd $(dirname $0)/files && 
+#         echo $config_name | 
+#         cpio --quiet -H newc -o -A -F $copy_dir/install.amd/initrd
+#     )
+#     gzip $copy_dir/install.amd/initrd
+#     chmod -w -R $copy_dir/install.amd/
 
-    if [ $distro == 'debian' ]; then
-        # Add firmware package to the iso
-        wget -q --show-progress -nc $iwlwifi -P $copy_dir/firmware/dep11
+#     if [ $distro == 'debian' ]; then
+#         # Add firmware package to the iso
+#         wget -q --show-progress -nc $iwlwifi -P $copy_dir/firmware/dep11
         
-        # Append boot argument so fdisk can be preloaded and used later in preseed
-        sed -i 's/--- quiet/modules=util-linux-udeb --- quiet/' $copy_dir/isolinux/txt.cfg
+#         # Append boot argument so fdisk can be preloaded and used later in preseed
+#         sed -i 's/--- quiet/modules=util-linux-udeb --- quiet/' $copy_dir/isolinux/txt.cfg
 
-        # Change splash screen shown on iso boot
-        cp "$(dirname $0)/files/splash.png" $copy_dir/isolinux/
-    fi
+#         # Change splash screen shown on iso boot
+#         cp "$(dirname $0)/files/splash.png" $copy_dir/isolinux/
+#     fi
 
-    # Add scripts used in preseed
-    cp -R "$(dirname $0)/../in-target" $copy_dir/
+#     # Add scripts used in preseed
+#     cp -R "$(dirname $0)/../in-target" $copy_dir/
 
-    # Recalculate md5 checksums and update the list
-    chmod +w $copy_dir/md5sum.txt
-    cd $copy_dir && find -follow -type f ! -name md5sum.txt -print0 | xargs -0 md5sum > md5sum.txt
-    chmod -w $copy_dir/md5sum.txt
+#     # Recalculate md5 checksums and update the list
+#     chmod +w $copy_dir/md5sum.txt
+#     cd $copy_dir && find -follow -type f ! -name md5sum.txt -print0 | xargs -0 md5sum > md5sum.txt
+#     chmod -w $copy_dir/md5sum.txt
 
-    # Generate new image and make it bootable
-    genisoimage -r -J -b isolinux/isolinux.bin \
-                -c isolinux/boot.cat \
-                -no-emul-boot \
-                -boot-load-size 4 \
-                -boot-info-table \
-                -o $copy_name \
-                $copy_dir
-    isohybrid $copy_name
+#     # Generate new image and make it bootable
+#     genisoimage -r -J -b isolinux/isolinux.bin \
+#                 -c isolinux/boot.cat \
+#                 -no-emul-boot \
+#                 -boot-load-size 4 \
+#                 -boot-info-table \
+#                 -o $copy_name \
+#                 $copy_dir
+#     isohybrid $copy_name
 
-else
-    echo "Aborting because of checksum mismatch!"
-fi
+# else
+#     echo "Aborting because of checksum mismatch!"
+# fi
